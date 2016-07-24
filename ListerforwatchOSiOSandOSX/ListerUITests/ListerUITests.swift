c06d59d3587c14ca6ab930feea131904bc1c2f45
Hanyu <rollr76518@gmail.com>
2016-07-24 16:44:32 +0800

    1. 調整部份 code

--- ListerforwatchOSiOSandOSX/ListerUITests/ListerUITests.swift
+++ ListerforwatchOSiOSandOSX/ListerUITests/ListerUITests.swift
@@ -8,6 +8,8 @@
 
 import XCTest
 
+let FIRST_CELL_TITLE = "Groceries"
+
 class ListerUITests: XCTestCase {
         
     override func setUp() {
@@ -35,50 +37,102 @@ class ListerUITests: XCTestCase {
     }
     
     func testDelete() {
-        
+        //初始化
         let app = XCUIApplication()
+        
+        //從第一頁進到第二頁
         let tablesQuery = app.tables
-        tablesQuery.staticTexts["Groceries"].tap()
         
-        let count = tablesQuery.cells.count
+        let firstCell = tablesQuery.staticTexts[FIRST_CELL_TITLE]
+        firstCell.tap()
         
-        let groceriesNavigationBarsQuery = app.navigationBars.matchingIdentifier("Groceries")
-        groceriesNavigationBarsQuery.buttons["Edit"].tap()
-        tablesQuery.childrenMatchingType(.Cell).elementBoundByIndex(1).buttons["Delete "].tap()
-        tablesQuery.buttons["Delete"].tap()
-        groceriesNavigationBarsQuery.buttons["Done"].tap()
+        //第二頁按編輯之後刪除
+        let orignalCount = tablesQuery.cells.count
         
-        XCTAssertEqual(tablesQuery.cells.count, count - 1)
+        let groceriesNavigationBarsQuery = app.navigationBars.matchingIdentifier(FIRST_CELL_TITLE)
+        let buttonEdit = groceriesNavigationBarsQuery.buttons["Edit"]
+        buttonEdit.tap()
+        
+        let firstItem = tablesQuery.childrenMatchingType(.Cell).elementBoundByIndex(1)
+        firstItem.buttons["Delete "].tap()
+        firstItem.buttons["Delete"].tap()
+         
+        let buttonDone = groceriesNavigationBarsQuery.buttons["Done"]
+        buttonDone.tap()
+
+        let newCount = tablesQuery.cells.count
+        
+        //檢查刪除前跟刪除後是否少1
+        XCTAssertEqual(newCount, orignalCount - 1)
     }
     
     func testAdd() {
+        //初始化
         let app = XCUIApplication()
-        let tablesQuery = app.tables
-        tablesQuery.staticTexts["Groceries"].tap()
         
-        let count = tablesQuery.cells.count
+        //從第一頁進到第二頁
+        let tablesQuery = app.tables
+        let firstCell = tablesQuery.staticTexts[FIRST_CELL_TITLE]
+        firstCell.tap()
+        
+        //第二頁按新增之後增加
+        let orignalCount = tablesQuery.cells.count
         
         let tableTextField = tablesQuery.textFields["Add Item"]
         tableTextField.tap()
         tableTextField.typeText("cookies")
         app.buttons["Done"].tap()
         
-        XCTAssertEqual(tablesQuery.cells.count, count + 1)
+        let newCount = tablesQuery.cells.count
+        
+        //檢查新增前跟新增後是否加1
+        XCTAssertEqual(newCount, orignalCount + 1)
     }
     
     func testColor() {
+        //初始化
         let app = XCUIApplication()
-        let tablesQuery = app.tables
-        tablesQuery.staticTexts["Groceries"].tap()
         
-        let groceriesNavigationBarsQuery = app.navigationBars.matchingIdentifier("Groceries")
+        //從第一頁進到第二頁
+        let tablesQuery = app.tables
+        let firstCell = tablesQuery.staticTexts[FIRST_CELL_TITLE]
+        firstCell.tap()
+        
+        //第二頁按編輯之後點擊各個 button(ID)
+        let groceriesNavigationBarsQuery = app.navigationBars.matchingIdentifier(FIRST_CELL_TITLE)
         groceriesNavigationBarsQuery.buttons["Edit"].tap()
-        tablesQuery.buttons["red"].tap()
+        let redButton = tablesQuery.buttons["red"]
+        redButton.tap()
+        //可以測試 identifier 是否正確，但無法測試顏色
+        XCTAssertEqual(redButton.identifier, "red")
+        
+//        XCUIApplication().tables.childrenMatchingType(.Cell).elementBoundByIndex(1).buttons["Reorder"].tap()
+        
         tablesQuery.buttons["orange"].tap()
         tablesQuery.buttons["yellow"].tap()
         tablesQuery.buttons["green"].tap()
         tablesQuery.buttons["blue"].tap()
         tablesQuery.buttons["black"].tap()
         groceriesNavigationBarsQuery.buttons["Done"].tap()
+    }
+    
+    func testForButton() {
+        
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Groceries"].tap()
+        
+        let groceriesNavigationBarsQuery = app.navigationBars.matchingIdentifier("Groceries")
+        groceriesNavigationBarsQuery.buttons["Edit"].tap()
+        
+        
+//        let reorderButton = XCUIApplication().tables.childrenMatchingType(.Cell).elementBoundByIndex(1).buttons["Reorder"]
+//        reorderButton.swipeUp()
+
+        tablesQuery.buttons["Oranges"].tap()
+//        groceriesNavigationBarsQuery.buttons["Done"].tap()
+        
+        
+        
     }
 }
